69ded6227dc42df91c0f650982d6d1b276e295d8
Slave Tanushev <slavetanushev@sourcemxacademy.mk>
2020-05-31 20:39:17 +0200

    Calculator done
    React simple calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeysComponent from "./components/KeysComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+
+    if (button === "=") {
+      this.calculate()
+    }
+
+    else if (button === "C") {
+      this.reset()
+    }
+    else if (button === "CE") {
+      this.backspace()
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+
+  calculate = () => {
+    let checkResult = ''
+    if (this.state.result.includes('--')) {
+      checkResult = this.state.result.replace('--', '+')
+    }
+
+    else {
+      checkResult = this.state.result
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+
+    }
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calculator-body">
+          <h1>React Calculator</h1>
+          <ResultComponent result={this.state.result} />
+          <KeysComponent onClick={this.onClick} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
